8b4400862dc1ddfae1246574074d61210236e03e
JonnyBoy04 <simonzdjgtz@gmail.com>
2024-08-15 12:37:41 -0600

    Monitoreo de ritmo cardiaco

--- wear/src/main/java/com/softli/health/GraficaActivity.kt
+++ wear/src/main/java/com/softli/health/GraficaActivity.kt
@@ -1,6 +1,12 @@
 package com.softli.health
 
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -8,7 +14,11 @@ import android.util.Log
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.google.android.gms.wearable.DataClient
 import com.google.android.gms.wearable.DataEvent
 import com.google.android.gms.wearable.DataEventBuffer
@@ -20,17 +30,26 @@ import com.google.android.gms.wearable.Wearable
 import com.softli.health.EmergenciaActivity
 import com.softli.health.R
 
-class GraficaActivity : ComponentActivity(), MessageClient.OnMessageReceivedListener {
+class GraficaActivity : ComponentActivity(), MessageClient.OnMessageReceivedListener, SensorEventListener {
 
     private lateinit var imgGrafica: ImageView
     private lateinit var barraProgreso: ProgressBar
     private lateinit var txtLatidos: TextView
+    private lateinit var sensorManager: SensorManager
+    private var heartRateSensor: Sensor? = null
 
-    private val handler = Handler(Looper.getMainLooper())
+    private val requestPermissionLauncher: ActivityResultLauncher<String> =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                Log.d("MainActivity", "Permiso de sensores corporales concedido")
+                initializeSensor()
+            } else {
+                Log.d("MainActivity", "Permiso de sensores corporales denegado")
+                Toast.makeText(this, "Permiso de sensores corporales denegado", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     private var currentIndex = 0
-    private val updateInterval = 1000L
-    private val heartRates =
-        intArrayOf(60, 62, 63, 65, 66, 68, 69, 71, 73, 75, 76, 78, 79, 81, 83, 84, 86, 87, 89)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,47 +59,50 @@ class GraficaActivity : ComponentActivity(), MessageClient.OnMessageReceivedList
         barraProgreso = findViewById(R.id.barraProgreso)
         txtLatidos = findViewById(R.id.txtLatidos)
 
+        when {
+            ContextCompat.checkSelfPermission(this, android.Manifest.permission.BODY_SENSORS) == PackageManager.PERMISSION_GRANTED -> {
+                Log.d("MainActivity", "Permiso de sensores corporales ya concedido")
+                initializeSensor()
+            }
+            else -> {
+                Log.d("MainActivity", "Solicitando permiso de sensores corporales")
+                requestPermissionLauncher.launch(android.Manifest.permission.BODY_SENSORS)
+            }
+        }
+
     }
 
     override fun onResume() {
         super.onResume()
-        handler.post(updateHeartRate)
+        Wearable.getMessageClient(this).addListener(this)
     }
 
     override fun onPause() {
         super.onPause()
-        handler.removeCallbacks(updateHeartRate)
+        Wearable.getMessageClient(this).removeListener(this)
     }
 
-    private val updateHeartRate = object : Runnable {
-        override fun run() {
-            if (currentIndex < heartRates.size) {
-                val heartRate = heartRates[currentIndex]
-                txtLatidos.text = "Frecuencia cardíaca: $heartRate"
-                barraProgreso.progress = heartRate
+    fun updateRate(heartRate: Int) {
+        Log.d("Runnable", "Actualizando UI con frecuencia cardíaca: $heartRate")
+        Log.d("Runnable","Frecuencia Cardíaca: $heartRate")
 
-                // Enviar el valor del pulso cardíaco continuamente
-                sendMessageToPhone("Frecuencia cardíaca: $heartRate")
+        txtLatidos.text = "Frecuencia cardíaca: $heartRate"
+        barraProgreso.progress = heartRate
 
-                // Si los latidos son 87 o más, iniciar la actividad de emergencia
-                if (heartRate >= 87) {
-                    val intent = Intent(this@GraficaActivity, EmergenciaActivity::class.java)
-                    startActivity(intent)
-                    finish()
-                } else {
-                    currentIndex++
-                    handler.postDelayed(this, updateInterval)
-                }
-            }
+        // Enviar el valor del pulso cardíaco continuamente
+        sendMessageToPhone("Frecuencia cardíaca: $heartRate")
+
+        // Si los latidos son 87 o más, iniciar la actividad de emergencia
+        if (heartRate >= 110) {
+            val intent = Intent(this@GraficaActivity, EmergenciaActivity::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            currentIndex++
         }
+
     }
 
-    override fun onMessageReceived(messageEvent: MessageEvent) {
-        if (messageEvent.path == "/recordatory") {
-            val message = String(messageEvent.data)
-            Log.d("Recordatory", "Message received: $message")
-        }
-    }
 
 
     private fun sendMessageToPhone(message: String) {
@@ -95,6 +117,63 @@ class GraficaActivity : ComponentActivity(), MessageClient.OnMessageReceivedList
                     }
             }
         }
+    }
+
+    override fun onMessageReceived(messageEvent: MessageEvent) {
+        if (messageEvent.path == "/recordatory") {
+            val message = String(messageEvent.data)
+            Log.d("Recordatory", "Message received: $message")
+
+            val regex = Regex("""(.+?) a las (.+?) con id:(\d+)""")
+            val matchResult = regex.find(message)
+
+            if (matchResult != null) {
+                val (medicamento, horaCompleta, idRecordatorio) = matchResult.destructured
+                val hora = horaCompleta.split("T")[1] // Solo la hora
+
+                val mensajeCorto = "$medicamento\n$hora"
+
+                val intent = Intent(this, RecordatorioActivity::class.java).apply {
+                    putExtra("recordatorio_message", mensajeCorto)
+                    putExtra("id_recordatorio", idRecordatorio.toInt()) // Pasar el ID como extra
+                }
+                startActivity(intent)
+            } else {
+                Log.e("Recordatory", "Formato de mensaje no válido: $message")
+            }
+        }
+    }
+
+
+    private fun initializeSensor() {
+        Log.d("MainActivity", "Inicializando el sensor de frecuencia cardíaca")
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        heartRateSensor = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE)
+        if (heartRateSensor != null) {
+            Log.d("MainActivity", "Sensor de frecuencia cardíaca encontrado, registrando listener")
+            sensorManager.registerListener(this, heartRateSensor, SensorManager.SENSOR_DELAY_NORMAL)
+        } else {
+            Log.d("MainActivity", "Sensor de frecuencia cardíaca no encontrado")
+        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event != null && event.sensor.type == Sensor.TYPE_HEART_RATE) {
+            val heartRate = event.values[0].toInt()
+            Log.d("MainActivity", "Frecuencia cardíaca recibida: $heartRate")
+            updateRate(heartRate)
+        } else {
+            Log.d("MainActivity", "Evento de sensor no es de tipo frecuencia cardíaca")
+        }
+    }
+
+    private fun updateHeartRateText(heartRate: Int) {
+        Log.d("MainActivity", "Actualizando UI con frecuencia cardíaca: $heartRate")
+        Log.d("MainActivity","Frecuencia Cardíaca: $heartRate")
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        Log.d("MainActivity", "onAccuracyChanged llamado, precisión: $accuracy")
     }
 
 }
